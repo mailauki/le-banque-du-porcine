00b0845dd936221e83c8e575d95a89e59b932e42
Julie Evans <juliemevans17@gmail.com>
2022-06-22 17:06:31 -0500

    Setup

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>Le Banque Du Porcine</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Switch>
+          <Route path="/testing">
+            <h1>Test Route</h1>
+          </Route>
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
